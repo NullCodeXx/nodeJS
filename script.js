1cf981b08db1f3ab08e6e90debe739d882fc9cac
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-23 10:30:04 +0200

    create file toto. txt width function writeFile and function readdir()

---
+++ script.js
@@ -0,0 +1,34 @@
+/**
+1. create a new file `script.js`
+2. in `script.js` write a program which:
+    - create a file `toto.txt`
+   - write `Salut Toto !` inside `toto.txt`
+3. _Bonus_ in `script.js` improve the program so that it can:
+    -  list and display the content of the current folder using synchronous functions
+    - list and display the content of the current folder using asynchronous functions
+ */
+//Librairie qui permet d'acceder a file systeme.
+ const fs = require('fs');
+
+//POUR CRÉE UN FICHIER .TXT
+
+fs.writeFile('toto.txt', function() {
+    if(err) {
+        console.error(err);
+    }
+    console.log('toto.txt à été crée');
+    //POUR LIRE LE CONTENUS D'UN DOSSIER.
+
+    fs.readdir('.', function(err, files) {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            console.log(files);
+        }
+    });
+});
+
+//Crée des function lambda : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions/Fonctions_fl%C3%A9ch%C3%A9es
+//Test in terminal : node script.js
+
